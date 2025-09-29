1c5fd8b5ba37e280cd319683f9aedb36b81c4de8
jeraku <mailsforjegan@gmail.com>
2025-09-29 07:06:22 +0100

    set code

--- python/week2/set.py
+++ python/week2/set.py
@@ -2,9 +2,28 @@
 avoid duplication
 '''
 items = {"apple","kiwi", "apple", "banana", "oragne", "grapde"} # employee details to maintain unique data
+
 print(items)
 items.add("jtest")
 print(items)
 items.remove("apple")
 print(items)
 items.discard("applew") #if I use remove it will throw error, discard will not throw error when vlaue is not present
+
+items1 = {"apple","kiwi", "apple", "banana1", "oragne1", "grapde"} # employee details to maintain unique data
+items = {"apple","kiwi", "apple", "banana", "oragne", "grapde"} # employee details to maintain unique data
+
+
+print(items.intersection(items1))
+print(items.union(items1))
+print(items.difference(items1))
+
+count =0
+while len(items) > count:
+    print(items) 
+    count = count+1
+
+
+
+
+
